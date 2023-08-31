8975bea6deecf363480f3c4381f45a25ca5e1679
Jose Ribeiro <joseribeirocsr@gmail.com>
2023-08-31 15:30:06 -0300

    cadastro de usuario, nota e link.

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors");
-const migrationsRun = require("./databese/sqlite/migrations");
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 
 const express = require("express");
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json())
 
 app.use(routes);
-
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
